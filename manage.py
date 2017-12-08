1a437275522df5f5ea851947125fa13eb1646f6e
baallee <34082563+baallee@users.noreply.github.com>
2017-12-08 16:44:16 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+import time
+from flask import Flask,g,request,make_response
+import hashlib
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
-
+app.debug=True
 
 @app.route('/')
 def login():
@@ -60,25 +63,31 @@ def grid():
 def stocks():
     return render_template('stocks.html')
 
-@app.route('/wxlogin')
-def wxlogin():
-    try:  
-        if request.method=='GET':
-            token='big_project'
-            data=request.args
-            signature=data.get('signature','')
-            timestamp=data.get('timestamp','')
-            nonce =data.get('nonce','')
-            echostr=data.get('echostr','')
-            s=[timestamp,nonce,token]
-            s.sort()
-            s=''.join(s)
-            if(hashlib.sha1(s).hexdigest()==signature):
-                return make_response(echostr)
-            else:  
-                return "login failed!"  
-    except Exception, Argument:  
-        return Argument 
+@app.route('/wxlogin',methods=['GET','POST'])
+def wechat_auth():
+    if request.method == 'GET':
+        token='big_project' 
+        data = request.args
+        signature = data.get('signature','')
+        timestamp = data.get('timestamp','')
+        nonce = data.get('nonce','')
+        echostr = data.get('echostr','')
+        s = [timestamp,nonce,token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
+    else:
+        rec = request.stream.read()
+        xml_rec = ET.fromstring(rec)
+        tou = xml_rec.find('ToUserName').text
+        fromu = xml_rec.find('FromUserName').text
+        content = xml_rec.find('Content').text
+        xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        response = make_response(xml_rep % (fromu,tou,str(int(time.time())), content))
+        response.content_type='application/xml'
+        return response
+    return 'Hello weixin!'
 
 if __name__ == '__main__':
     app.run()

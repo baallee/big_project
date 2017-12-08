984b64da9506e7f9bee4af0d96f22310940a1fd8
baallee <lee.baal@gmail.com>
2017-12-08 17:29:38 +0800

    remove unuse import

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-from flask import Flask,g,request,render_template,make_response
+from flask import Flask,request,render_template,make_response
 import time
 import hashlib
 import xml.etree.ElementTree as ET
